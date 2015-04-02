2ac52faf5b397ce4c03451a7a74f2b7f09495735
cheble <chase.heble@gmail.com>
2015-04-02 18:33:02 -0500

    testing

--- sphere/sphere.js
+++ sphere/sphere.js
@@ -234,7 +234,9 @@ window.onload = function init() {
 
 function initSphere() {
   // Load shaders and initialize attribute buffers
-  theSphereProgram = initShaders(gl, "sphere-vertex-shader", "sphere-fragment-shader");
+  // theSphereProgram = initShaders(gl, "sphere-vertex-shader", "sphere-fragment-shader");
+  // var myUtils = utils;
+  theSphereProgram = utils.addShaderProg(gl, 'sphere_vertex.shader', 'sphere_frag.shader');
   gl.useProgram(theSphereProgram);
 
   // Create VBOs and load the data into the VBOs
